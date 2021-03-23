d32d4ba96cdf854495c2c7254335f8d3574e6bcb
Nicu Reut <reutnicu@gmail.com>
2021-03-23 10:06:55 +0100

    remove file restriction

--- src/dropzone/Dropzone.js
+++ src/dropzone/Dropzone.js
@@ -77,11 +77,6 @@ const Dropzone = () => {
     }
 
     const validateFile = (file) => {
-        const validTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/x-icon'];
-        if (validTypes.indexOf(file.type) === -1) {
-            return false;
-        }
-
         return true;
     }
 
