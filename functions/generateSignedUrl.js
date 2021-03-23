aaa4d270f2ed295df317e3beadb33a1f92b78b47
Nicu Reut <contact@nicu.dev>
2021-03-23 18:14:34 +0100

    add stupid cors

--- functions/generateSignedUrl.js
+++ functions/generateSignedUrl.js
@@ -4,22 +4,35 @@ const { Storage } = require("@google-cloud/storage");
 // Creates a client
 const storage = new Storage();
 exports.generateSignedUrl = async (req, res) => {
-  const options = {
-    version: "v4",
-    action: "write",
-    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
-    contentType: "application/octet-stream",
-  };
+  // Allows GETs from any origin with the Content-Type header
+  // and caches preflight response for 3600s
 
-  // Get a v4 signed URL for uploading file
-  try {
-    const [url] = await storage
-      .bucket(BUCKET)
-      .file(req.body.name)
-      .getSignedUrl(options);
-    res.status(200).send(url);
-  } catch (e) {
-    console.log(e);
-    res.send(500);
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+  } else {
+    const options = {
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+      contentType: "application/octet-stream",
+    };
+
+    // Get a v4 signed URL for uploading file
+    try {
+      const [url] = await storage
+        .bucket(BUCKET)
+        .file(req.body.name)
+        .getSignedUrl(options);
+      res.status(200).send(url);
+    } catch (e) {
+      console.log(e);
+      res.send(500);
+    }
   }
 };
