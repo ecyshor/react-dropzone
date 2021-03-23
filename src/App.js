a71c91045b0fb0901bd4f6f7f2db23ba2e6868c5
Nicu Reut <reutnicu@gmail.com>
2021-03-23 10:57:45 +0100

    Add guard with current user

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 import './App.css';
 import Dropzone from "./dropzone/Dropzone";
+import identity from "netlify-identity-widget";
+identity.init();
 
 function App() {
+  if(identity.currentUser())
   return (
     <div>
-      <p className="title">React Drag and Drop Image Upload</p>
+      <p className="title">Upload</p>
       <div className="content">
         <Dropzone />
       </div>
     </div>
-  );
+  );else {
+    identity.open('login');
+  }
 }
 
 export default App;
